55295a8fb77817f8918e988f0b32ac9eb8b35665
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-28 12:29:23 +0200

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -13,6 +13,8 @@ async def on_redy():
     print( 'Bot connected')
 
 
+
+    
 @client.command()
 async def start( ctx ):
     await ctx.channel.purge( limit = 1 )
@@ -35,8 +37,17 @@ async def start( ctx ):
         await asyncio.sleep(7200)
         await ctx.send(f"!d bump")
         await asyncio.sleep(7200)
-
     
+
+@client.command()
+async def start( ctx ):
+    await ctx.channel.purge( limit = 1 )
+    while True:
+        a = random.choice(['эй','привет','алло','девять чи десять?','ку','аваава','как дела?','упс','ДЕВЯТЬ???','ало','алооо','здарова'])
+        await ctx.send(a)
+        t = random.choice(['5','10','100','24','20','40','120'])
+        await asyncio.sleep(t)
+        
     
     
 token= os.environ.get('BOT_TOKEN')
