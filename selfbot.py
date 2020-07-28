6e7374fa9d1c39507ee3f8b69898c57a65d36aa0
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-28 18:20:10 +0200

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -46,19 +46,6 @@ async def priv( ctx ):
         await asyncio.sleep(20)
         
 
-#coin
-@client.command()
-async def coin(ctx):
-    await ctx.channel.purge(limit = 1)
-    coins = [ 'орел', 'решка' ]
-    coins_r = random.choice( coins )
-    coin_win = 'орел'
-    if coins_r == coin_win:
-        emb = discord.Embed(description= f''':tada: { ctx.message.author.name }, выиграл! \nТебе повезло у тебя: ``{ coins_r }``''', color = 0x0c0c0c)
-        await ctx.send(embed = emb)
-    if coins_r != coin_win:
-        emb = discord.Embed(description= f''':thumbsdown:  { ctx.message.author.name }, проиграл!\n Тебе не повезло у тебя: ``{ coins_r }``''', color = 0x0c0c0c)
-        await ctx.send(embed = emb)
 
     
 token= os.environ.get('BOT_TOKEN')
